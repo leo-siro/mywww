181b3db358a546776f1e2b89e98d76debf0679dd
tateo.shiroma <tateo_shiroma@leopalace21.com>
2021-04-27 17:40:18 +0900

    deplytest

--- mywwwdeply.php
+++ mywwwdeply.php
@@ -7,16 +7,18 @@ $bare_path = parse_url($url, PHP_URL_PATH);
 $b = preg_replace('|/git|', '/home/webadmin/.gitbucket/repositories', $bare_path);
 $nb = preg_replace('|/git|', '/home/webadmin/.gitbucket/nonbare', $bare_path);
 $nb = preg_replace('/\.git$/', '', $nb);
+// `rm -rf $nb`;
 if (!file_exists($nb)) {
-  `git clone $b $nb > "./tmp/test.txt" 2>&1`;
+  `git clone $b $nb 2>&1`;
 } else {
-  `cd $nb && git pull > "./tmp/test.txt" 2>&1`;
+  `cd $nb && git pull`;
+//   `git pull > ./tmp/test.txt 2>&1`;
 }
 // lftpで送信（FTPで配置する場合）
-$host = 'localhost';
-$user = 'webadmin';
-$pass = 'leo';
-$rdir = '/srv/www/htdocs_mywwwdev/test';
-`lftp -u $user,$pass -e "mirror -Rn -X .git/ $nb/ $rdir; bye" $host 2>&1`;
+// $host = 'localhost';
+// $user = 'webadmin';
+// $pass = 'leo';
+// $rdir = '/srv/www/htdocs_mywwwdev/test';
+// `lftp -u $user,$pass -e "mirror -Rn -X .git/ $nb/ $rdir; bye" $host 2>&1`;
 // file_put_contents("./tmp/test.txt","lftp -u {$user},{$pass} -e \"mirror -Rev -X .git/ {$nb}/ {$rdir}; bye\" {$host} 2>&1" );
 ?>
